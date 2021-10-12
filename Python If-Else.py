cc5f3f6ecd90100dd2862e69a05c75cd2c5e9dbb
Programming_Bhau <59693249+jrmluvsu@users.noreply.github.com>
2021-10-12 11:37:33 +0530

    Add files via upload

---
+++ Python If-Else.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+num=int(input())
+if num%2!=0:
+    print("Weird")
+else:
+    if num>=2 and num<=5:
+        print("Not Weird")
+    elif num>=6 and num<=20:
+        print("Weird")
+    else:
+        print("Not Weird")
